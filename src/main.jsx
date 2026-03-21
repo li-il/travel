a30d83313a21ff1b5d21a1dc3f3a5286081852e1
L <l.l@L.local>
2026-03-21 01:01:02 -0400

    CHORE: Absolute fix for App component definition

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 确保这里的小点和斜杠正确，且 App 的首字母大写
-import App from './App,jsx' 
+import App from './App.jsx' 
 import './index.css'
 
 const rootElement = document.getElementById('root');
